445722331f8909c7cdb6f0c38018b85b6f017e09
Đinh Mạnh Hải <haidm@thinhtientech.com>
2022-05-29 00:28:02 +0700

    delete note

--- lib/app/pages/details/details_view.dart
+++ lib/app/pages/details/details_view.dart
@@ -10,6 +10,7 @@ import 'package:mobile/app/data/theme/theme.dart';
 import 'package:mobile/app/model/note.dart';
 import 'package:sizer/sizer.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'details_cubit.dart';
 
@@ -95,17 +96,20 @@ class _DetailsViewState extends State<DetailsView> {
                     ),
                     Row(
                       children: [
-                        Container(
-                          margin: EdgeInsets.only(right: 4.w),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: CustomColors.yellowText,
-                              width: 2,
+                        widget.data?.id != null ? InkWell(
+                          onTap: () => _showDialog(),
+                          child: Container(
+                            margin: EdgeInsets.only(right: 4.w),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: CustomColors.yellowText,
+                                width: 2,
+                              ),
+                              shape: BoxShape.circle,
                             ),
-                            shape: BoxShape.circle,
+                            child: const Icon(Icons.more_horiz_sharp, color: CustomColors.yellowText, size: 18),
                           ),
-                          child: const Icon(Icons.more_horiz_sharp, color: CustomColors.yellowText, size: 18),
-                        ),
+                        ) : const SizedBox(),
                         InkWell(
                           onTap: () => _cubit.createNote(widget.data?.id),
                           child: Text(
@@ -129,6 +133,83 @@ class _DetailsViewState extends State<DetailsView> {
                   }
                   return const SizedBox();
                 },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  void _showDialog() {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => Material(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: const Radius.circular(15)),
+        child: Container(
+          height: 50.h,
+          width: 100.w,
+          padding: EdgeInsets.all(2.w),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      InkWell(
+                        // onTap: () => _cubit.changeView(),
+                        child: Icon(
+                          Icons.folder,
+                          color: CustomColors.yellowText,
+                          size: 9.h,
+                        ),
+                      ),
+                      Text(
+                        'Tất cả ghi chú',
+                        style: titleTextStyle.copyWith(
+                          color: CustomColors.regularText,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () => Navigator.pop(context),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: SvgPicture.asset(
+                          "assets/x-icon.svg",
+                          height: 4.h,
+                          fit: BoxFit.fitHeight,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 4.h,
+              ),
+              InkWell(
+                onTap: () => _cubit.buttonDeleteNote(widget.data!.id!),
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 2.h, horizontal: 2.w),
+                  decoration: BoxDecoration(
+                    color: CustomColors.greyText,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Xóa ghi chú',
+                        style: titleTextStyle.copyWith(color: CustomColors.regularText),
+                      ),
+                      Icon(Icons.delete, color: CustomColors.regularText,),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
